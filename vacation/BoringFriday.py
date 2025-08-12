6af3f246a6f96bb970570766ad475811e1e3856e
Taegyun Kim <kimtg2004@gmail.com>
2025-08-12 10:13:21 +0900

    경기도 광주 기온 검색 최종

--- vacation/BoringFriday.py
+++ vacation/BoringFriday.py
@@ -4,14 +4,11 @@ with (open("kyong_gi_gwank_zu_weather_data.csv", "r") as f): #city_name, yyyy-mm
 
     #elements
     Year = (2023, 2024)
-    Months = list(range(1,13))
+    Months = list(range(0,13))
     Day = list(range(0,32))
 
-    # flag
-    def flag():
-        flag_ = input("\nIf you want to quit the program,"
-                      "\nplease input 'c', the others will continue the program:")
-        return flag_
+
+
 
     # error_logs
     def error_wrong_date():
@@ -35,11 +32,11 @@ with (open("kyong_gi_gwank_zu_weather_data.csv", "r") as f): #city_name, yyyy-mm
             if int(choose_date[6:7]) not in Months[:10]:
                 error_wrong_date()
                 sleep(1.5)
-        elif choose_date[9:11] not in str(Day):
+        elif choose_date[8:10] not in str(Day):
             error_wrong_date()
             sleep(1.5)
-        elif choose_date[6:8] in ('02', '04', '06', '09', '11'):
-            if choose_date[9:11] >= '31':
+        elif choose_date[5:7] in ('02', '04', '06', '09', '11'):
+            if choose_date[8:10] >= '31':
                 error_wrong_date()
                 sleep(1.5)
 
@@ -57,7 +54,11 @@ with (open("kyong_gi_gwank_zu_weather_data.csv", "r") as f): #city_name, yyyy-mm
                 print("\nIt is wrong form, please write in correct form")
                 sleep(1)
                 break
-            flag()
-            if flag == 'c' or 'C':
-                break
+        flag_ = input("\nIf you want to quit the program,"
+                      "\nplease input 'c', the others will continue the program:")
+        if flag_.strip().lower() == 'c':
+            break
+        else:
+            continue
+
 
