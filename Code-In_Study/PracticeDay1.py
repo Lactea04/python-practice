552cf8ed7ae03fa2470118e24071f136d762bbfb
Taegyun Kim <kimtg2004@gmail.com>
2025-10-02 10:23:47 +0900

    [feat] code-in 스터디 대비 코딩 100제 day1

---
+++ Code-In_Study/PracticeDay1.py
@@ -0,0 +1,35 @@
+#6001
+#print("Hello")
+
+#6002
+#print("hello world".title())
+
+#6003
+#print("hello\nworld".title())
+
+#6004
+#print("'hello'".title())
+
+#6005
+#print('"hello world"'.title())
+
+#6006
+#print('"!@#$%^&*()\'') #\' 혹은 \"를 이용하여 따옴표 안에서 따옴표 표현 가능
+
+#6007
+#print("\"C:\\Download\\'hello'.py\"")
+
+#6008
+#print("print(\"Hello\\nWorld\")")
+
+#6009
+#c = "a"
+#print(c)
+
+#6010
+#n = 1203
+#n = int(n)
+#print(n)
+
+
+
